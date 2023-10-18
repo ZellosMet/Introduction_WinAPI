058a795af0a5967389f8828b3d04aac2d40fc7fb
Zello <Zello@DESKTOP-IBMII35>
2023-10-18 23:16:46 +0900

    Add project files.

---
+++ WinAPI/main.cpp
@@ -0,0 +1,7 @@
+#include<Windows.h>
+
+INT WINAPI WinMain(HINSTANCE hinstance, HINSTANCE hPrevInst, LPSTR lpCmdLine, INT nCmdShow)
+{
+	MessageBox(NULL, "Hello Windows!", "Header", MB_YESNOCANCEL | MB_ICONINFORMATION | MB_HELP | MB_ICONERROR);
+	return 0;
+}

8df138415c546a3f2fe848a9d9058e31c5f8e888
Professional <Professional@USER-PC>
2023-10-26 15:00:17 +0900

    MainWindow DONE

--- MainWindow/main.cpp
+++ MainWindow/main.cpp
@@ -1,12 +1,16 @@
+#define _CRT_SECURE_NO_WARNINGS
 #include<Windows.h>
+#include<stdio.h>
 #include"resource.h"
 
 CONST CHAR g_sz_WINDOW_CLASS[] = "My Window Class"; //Имя класса окна
 
 LRESULT CALLBACK WndProc(HWND hwnd, UINT uMsg, WPARAM wParem, LPARAM lParam);
+void MoveSizeEvent(HWND hwnd);
 
 INT WINAPI WinMain(HINSTANCE hInstance, HINSTANCE hPrevInst, LPSTR lpCmdLine, INT nCmdShow)
 {
+
 	// Главное окно создаётся в 3 этапа:
 	// 1) Регистрация окна;
 	WNDCLASSEX wc;
@@ -17,8 +21,12 @@ INT WINAPI WinMain(HINSTANCE hInstance, HINSTANCE hPrevInst, LPSTR lpCmdLine, IN
 	wc.cbClsExtra = 0; // Дополнительный байты класса окна
 	wc.style = 0;
 	
-	wc.hIcon = LoadIcon(hInstance, MAKEINTRESOURCE(IDI_ICON_RECYCLE)); // Иконка в панели задач
-	wc.hIconSm = LoadIcon(hInstance, MAKEINTRESOURCE(IDI_ICON_WALLPAPER)); //Sm -Small. Иконка в заголовке окна
+	//wc.hIcon = LoadIcon(hInstance, MAKEINTRESOURCE(IDI_ICON_RECYCLE)); // Иконка в панели задач
+	//wc.hIconSm = LoadIcon(hInstance, MAKEINTRESOURCE(IDI_ICON_WALLPAPER)); //Sm -Small. Иконка в заголовке окна
+	wc.hIcon = ExtractIcon(hInstance, "napster.ico", 0);
+	wc.hIconSm = ExtractIcon(hInstance, "mailchimp.ico", 0);
+
+
 	wc.hCursor = LoadCursor(NULL, IDC_ARROW);
 	wc.hbrBackground = HBRUSH(COLOR_WINDOW + 1);
 
@@ -44,8 +52,10 @@ INT WINAPI WinMain(HINSTANCE hInstance, HINSTANCE hPrevInst, LPSTR lpCmdLine, IN
 		
 		CW_USEDEFAULT, //Начальная позиция окна по Х
 		CW_USEDEFAULT, //Начальная позиция по Y
-		CW_USEDEFAULT, //Начальная позиция окна по Х
-		CW_USEDEFAULT, //Длина окна
+		GetSystemMetrics(SM_CXSCREEN)*3/4,
+		GetSystemMetrics(SM_CYSCREEN)*3/4,
+		//CW_USEDEFAULT, //Ширина окна
+		//CW_USEDEFAULT, //Длина окна
 		
 		NULL, //Родитель окна данного окна
 		NULL, //hMenu - Строка меню окна( NULL - не имеет). Для дочернего окна, который являкется элементом другого окна, в hMenu передаётся ID ресурса этого элемента.
@@ -76,13 +86,25 @@ LRESULT CALLBACK WndProc(HWND hwnd, UINT uMsg, WPARAM wParam, LPARAM lParam)
 {
 	switch(uMsg)
 	{
-		case WM_CREATE:
-			break;
-		case WM_COMMAND:
-			break;
+		case WM_CREATE:		break;
+		case WM_SIZE:		MoveSizeEvent(hwnd); break;
+		case WM_MOVE:		MoveSizeEvent(hwnd); break;
+		case WM_COMMAND:	break;
 		case WM_DESTROY:	PostQuitMessage(0); break;
 		case WM_CLOSE:		DestroyWindow(hwnd); break;
 		default:			return DefWindowProc(hwnd, uMsg, wParam, lParam);
 	}
 	return NULL;
+}
+
+void MoveSizeEvent(HWND hwnd)
+{
+	RECT rect;
+	GetWindowRect(hwnd, &rect);
+	UINT width = rect.right - rect.left;
+	UINT height = rect.bottom - rect.top;
+	CONST INT SIZE = 256;
+	CHAR sz_buffer[SIZE] = {};
+	sprintf(sz_buffer, "%s: Положение окна (%i x %i) Размер окна (%i x %i)", g_sz_WINDOW_CLASS, rect.left, rect.top, width, height);
+	SendMessage(hwnd, WM_SETTEXT, 0, (LPARAM)sz_buffer);
 }

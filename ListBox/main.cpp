fef15af681cb9bbdbad4520b1fbf90c67b4fd989
Professional <Professional@USER-PC>
2023-10-27 11:17:47 +0900

    ListBox DONE

--- ListBox/main.cpp
+++ ListBox/main.cpp
@@ -3,9 +3,11 @@
 #include"resource.h"
 #include<cstdio>
 
-CONST CHAR* g_sz_VALUES[] = { "This", "is", "my", "first", "List", "Box" };
+//CONST CHAR* g_sz_VALUES[] = { "This", "is", "my", "first", "List", "Box" };
 
 BOOL CALLBACK DlgProc(HWND hwnd, UINT uMsg, WPARAM wParam, LPARAM lParam);
+BOOL CALLBACK AddProc(HWND hwnd, UINT uMsg, WPARAM wParam, LPARAM lParam);
+BOOL CALLBACK DelProc(HWND hwnd, UINT uMsg, WPARAM wParam, LPARAM lParam);
 
 INT WINAPI WinMain(HINSTANCE hInstance, HINSTANCE hPrevInst, LPSTR lpCmdLine, INT nCmdShow)
 {
@@ -21,13 +23,23 @@ BOOL CALLBACK DlgProc(HWND hwnd, UINT uMsg, WPARAM wParam, LPARAM lParam)
 	{
 		HICON hIcon = LoadIcon(GetModuleHandle(NULL), MAKEINTRESOURCE(IDI_ICON1));
 		SendMessage(hwnd, WM_SETICON, 0, (LRESULT)hIcon);
-		for (int i = 0; i < sizeof(g_sz_VALUES) / sizeof(g_sz_VALUES[0]); i++)
-			SendMessage(GetDlgItem(hwnd, IDC_LIST1), LB_ADDSTRING, 0, (LPARAM)g_sz_VALUES[i]);
+		//for (int i = 0; i < sizeof(g_sz_VALUES) / sizeof(g_sz_VALUES[0]); i++)
+			//SendMessage(GetDlgItem(hwnd, IDC_LIST1), LB_ADDSTRING, 0, (LPARAM)g_sz_VALUES[i]);
 	}
 	break;
 	case WM_COMMAND:
 		switch (LOWORD(wParam))
 		{
+		case IDC_ADD:
+		{
+			DialogBoxParam(GetModuleHandle(NULL), MAKEINTRESOURCE(IDD_DIALOG_ADD), hwnd, (DLGPROC)AddProc, 0);
+		}
+		break;
+		case IDC_DELETE:
+		{
+			DialogBoxParam(GetModuleHandle(NULL), MAKEINTRESOURCE(IDD_DIALOG_DEL), hwnd, (DLGPROC)DelProc, 0);
+		}
+		break;
 		case IDOK:	
 		{
 			const int SIZE = 256;
@@ -42,6 +54,59 @@ BOOL CALLBACK DlgProc(HWND hwnd, UINT uMsg, WPARAM wParam, LPARAM lParam)
 		case IDCANCEL:	EndDialog(hwnd, 0); break;
 		}
 	break;
+	case WM_CLOSE: EndDialog(hwnd, 0);
+	}
+	return FALSE;
+}
+
+BOOL CALLBACK AddProc(HWND hwnd, UINT uMsg, WPARAM wParam, LPARAM lParam)
+{
+	switch (uMsg)
+	{
+	case WM_INITDIALOG:	break;
+	case WM_COMMAND:
+		switch (LOWORD(wParam))
+		{
+		case IDC_BUTTON_ADD:
+			{
+				CONST INT SIZE = 256;
+				CHAR sz_buffer[SIZE] = {};
+				HWND hParent = GetParent(hwnd);
+				HWND hList = GetDlgItem(hParent, IDC_LIST1);
+				HWND hEdit = GetDlgItem(hwnd, IDC_EDIT_ELEMENT);
+				SendMessage(hEdit, WM_GETTEXT, SIZE, (LPARAM)sz_buffer);
+				SendMessage(hList, LB_ADDSTRING, 0, (LPARAM)sz_buffer);
+				EndDialog(hwnd, 0);
+			}
+		break;
+		case IDC_BUTTON_CANCEL: EndDialog(hwnd, 0); break;
+		}
+	break;
+	case WM_CLOSE: EndDialog(hwnd, 0); 
+	}
+	return FALSE;
+}
+
+BOOL CALLBACK DelProc(HWND hwnd, UINT uMsg, WPARAM wParam, LPARAM lParam)
+{
+	switch (uMsg)
+	{
+	case WM_INITDIALOG:	break;
+	case WM_COMMAND:
+		switch (LOWORD(wParam))
+		{
+		case IDOK:
+			{
+				CONST INT SIZE = 256;
+				CHAR sz_buffer[SIZE] = {};
+				HWND hParent = GetParent(hwnd);
+				HWND hList = GetDlgItem(hParent, IDC_LIST1);
+				int i = SendMessage(hList, LB_GETCURSEL, 0, 0);
+				SendMessage(hList, LB_DELETESTRING, i, (LPARAM)sz_buffer);
+			}
+		break;
+		}
+	case IDCANCEL: EndDialog(hwnd, 0);
 	case WM_CLOSE: EndDialog(hwnd, 0);
 	}
 	return FALSE;
--- ListBox/resource.h
+++ ListBox/resource.h
@@ -4,16 +4,22 @@
 //
 #define IDD_DIALOG1                     101
 #define IDI_ICON1                       103
+#define IDD_DIALOG_ADD                  104
+#define IDD_DIALOG_DEL                  106
 #define IDC_LIST1                       1001
-#define IDC_STATICTEXT                  1002
+#define IDC_ADD                         1003
+#define IDC_DELETE                      1004
+#define IDC_EDIT_ELEMENT                1005
+#define IDC_BUTTON_ADD                  1009
+#define IDC_BUTTON_CANCEL               1010
 
 // Next default values for new objects
 // 
 #ifdef APSTUDIO_INVOKED
 #ifndef APSTUDIO_READONLY_SYMBOLS
-#define _APS_NEXT_RESOURCE_VALUE        104
+#define _APS_NEXT_RESOURCE_VALUE        108
 #define _APS_NEXT_COMMAND_VALUE         40001
-#define _APS_NEXT_CONTROL_VALUE         1003
+#define _APS_NEXT_CONTROL_VALUE         1011
 #define _APS_NEXT_SYMED_VALUE           101
 #endif
 #endif

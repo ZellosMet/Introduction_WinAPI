1a20216e2d856dcc4397af22b89eda317bce5197
Zello <Zello@DESKTOP-IBMII35>
2023-10-28 23:28:07 +0900

    Cursor is Worked

--- MainWindow/main.cpp
+++ MainWindow/main.cpp
@@ -3,6 +3,10 @@
 #include<stdio.h>
 #include"resource.h"
 
+#define IDC_COMBO			1001
+#define IDC_BUTTON_APPLY	1002
+
+CONST CHAR* g_CURSOR[] = { "Busy.ani", "Normal Select.ani", "Working In Background.ani", "Move.ani"};
 CONST CHAR g_sz_WINDOW_CLASS[] = "My Window Class"; //Имя класса окна
 
 LRESULT CALLBACK WndProc(HWND hwnd, UINT uMsg, WPARAM wParem, LPARAM lParam);
@@ -27,7 +31,7 @@ INT WINAPI WinMain(HINSTANCE hInstance, HINSTANCE hPrevInst, LPSTR lpCmdLine, IN
 	wc.hIconSm = ExtractIcon(hInstance, "mailchimp.ico", 0);
 
 
-	//wc.hCursor = LoadCursor(NULL, IDC_ARROW);	
+	wc.hCursor = LoadCursor(NULL, IDC_ARROW);	
 	wc.hCursor = (HCURSOR)LoadImage(hInstance, "Link_Select.ani", IMAGE_CURSOR, LR_DEFAULTSIZE, LR_DEFAULTSIZE, LR_LOADFROMFILE);
 	wc.hbrBackground = HBRUSH(COLOR_WINDOW + 1);
 	wc.hInstance = hInstance; //hInstance - это экземпляр исполняемого файла программы в памяти
@@ -84,12 +88,50 @@ INT WINAPI WinMain(HINSTANCE hInstance, HINSTANCE hPrevInst, LPSTR lpCmdLine, IN
 
 LRESULT CALLBACK WndProc(HWND hwnd, UINT uMsg, WPARAM wParam, LPARAM lParam)
 {
+	static HCURSOR hCursor;
 	switch(uMsg)
 	{
-		case WM_CREATE:		break;
+		case WM_CREATE:		
+		{
+			HWND hCombo = CreateWindowEx(NULL, "ComboBox", "",	WS_CHILD | WS_VISIBLE | CBN_DROPDOWN, 10, 10, 200, 200, hwnd, (HMENU)IDC_COMBO, GetModuleHandle(NULL), NULL);
+			for (int i = 0; i < sizeof(g_CURSOR) / sizeof(g_CURSOR[0]); i++)
+				SendMessage(hCombo, CB_ADDSTRING, 0, (LPARAM)g_CURSOR[i]);
+			HWND hButton = CreateWindowEx(NULL, "Button", "Apply", WS_CHILD | WS_VISIBLE | BS_PUSHBUTTON, 220, 10, 100, 24, hwnd, (HMENU)IDC_BUTTON_APPLY, GetModuleHandle(NULL), NULL);
+		}
+		break;
 		case WM_SIZE:		MoveSizeEvent(hwnd); break;
 		case WM_MOVE:		MoveSizeEvent(hwnd); break;
-		case WM_COMMAND:	break;
+		case WM_SETCURSOR:
+		{
+			//HWND hCombo = GetDlgItem(hwnd, IDC_COMBO);
+			//int i = SendMessage(hCombo, CB_GETCURSEL, 0, 0);
+			//CHAR sz_filename[_MAX_FNAME]{};
+			//CHAR sz_filepath[_MAX_PATH] = "starcraft-original\\";
+			//SendMessage(hCombo, CB_GETLBTEXT, i, (LPARAM)sz_filename);
+			//strcat(sz_filepath, sz_filename);
+			//hCursor = (HCURSOR)LoadImage(GetModuleHandle(NULL), sz_filepath, IMAGE_CURSOR, LR_DEFAULTSIZE, LR_DEFAULTSIZE, LR_LOADFROMFILE);
+			SetCursor(hCursor);
+		}
+		break;
+		case WM_COMMAND:	
+		{
+			switch (LOWORD(wParam))
+			{
+			case IDC_BUTTON_APPLY:
+			{
+				HWND hCombo = GetDlgItem(hwnd, IDC_COMBO);
+				int i = SendMessage(hCombo, CB_GETCURSEL, 0, 0);
+				CHAR sz_filename[_MAX_FNAME]{};
+				CHAR sz_filepath[_MAX_PATH] = "starcraft-original\\";
+				SendMessage(hCombo, CB_GETLBTEXT, i, (LPARAM)sz_filename);
+				strcat(sz_filepath, sz_filename);
+				hCursor = (HCURSOR)LoadImage(GetModuleHandle(NULL), sz_filepath, IMAGE_CURSOR, LR_DEFAULTSIZE, LR_DEFAULTSIZE, LR_LOADFROMFILE);
+				SetCursor(hCursor);
+			}
+			break;
+			}
+		}
+			break;
 		case WM_DESTROY:	PostQuitMessage(0); break;
 		case WM_CLOSE:		DestroyWindow(hwnd); break;
 		default:			return DefWindowProc(hwnd, uMsg, wParam, lParam);

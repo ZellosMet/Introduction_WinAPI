860aae7637a0aa9007af86a6479034adc6892405
Zello <Zello@DESKTOP-IBMII35>
2023-10-30 22:30:52 +0900

    Cursor DONE

--- MainWindow/main.cpp
+++ MainWindow/main.cpp
@@ -1,13 +1,20 @@
 #define _CRT_SECURE_NO_WARNINGS
 #include<Windows.h>
+#include<WinUser.h>
 #include<stdio.h>
 #include"resource.h"
+#include<vector>
+#include<string>
+
 
 #define IDC_COMBO			1001
 #define IDC_BUTTON_APPLY	1002
 
-CONST CHAR* g_CURSOR[] = { "Busy.ani", "Normal Select.ani", "Working In Background.ani", "Move.ani"};
+//CONST CHAR* g_CURSOR[] = { "Busy.ani", "Normal Select.ani", "Working In Background.ani", "Move.ani"};
 CONST CHAR g_sz_WINDOW_CLASS[] = "My Window Class"; //Имя класса окна
+
+
+std::vector<std::string> LoadCursorFromDir(const std::string& directory);
 
 LRESULT CALLBACK WndProc(HWND hwnd, UINT uMsg, WPARAM wParem, LPARAM lParam);
 void MoveSizeEvent(HWND hwnd);
@@ -93,26 +100,15 @@ LRESULT CALLBACK WndProc(HWND hwnd, UINT uMsg, WPARAM wParam, LPARAM lParam)
 	{
 		case WM_CREATE:		
 		{
-			HWND hCombo = CreateWindowEx(NULL, "ComboBox", "",	WS_CHILD | WS_VISIBLE | CBN_DROPDOWN, 10, 10, 200, 200, hwnd, (HMENU)IDC_COMBO, GetModuleHandle(NULL), NULL);
-			for (int i = 0; i < sizeof(g_CURSOR) / sizeof(g_CURSOR[0]); i++)
-				SendMessage(hCombo, CB_ADDSTRING, 0, (LPARAM)g_CURSOR[i]);
+			HWND hCombo = CreateWindowEx(NULL, "ComboBox", "",	WS_CHILD | WS_VISIBLE | CBS_DROPDOWN | WS_VSCROLL, 10, 10, 200, 200, hwnd, (HMENU)IDC_COMBO, GetModuleHandle(NULL), NULL);
+			std::vector<std::string> cursor = LoadCursorFromDir("starcraft-original\\*");
+			for (int i = 0; i < cursor.size(); i++)
+				SendMessage(hCombo, CB_ADDSTRING, 0, (LPARAM)cursor[i].c_str());
 			HWND hButton = CreateWindowEx(NULL, "Button", "Apply", WS_CHILD | WS_VISIBLE | BS_PUSHBUTTON, 220, 10, 100, 24, hwnd, (HMENU)IDC_BUTTON_APPLY, GetModuleHandle(NULL), NULL);
 		}
 		break;
 		case WM_SIZE:		MoveSizeEvent(hwnd); break;
 		case WM_MOVE:		MoveSizeEvent(hwnd); break;
-		//case WM_SETCURSOR:
-		//{
-		//	//HWND hCombo = GetDlgItem(hwnd, IDC_COMBO);
-		//	//int i = SendMessage(hCombo, CB_GETCURSEL, 0, 0);
-		//	//CHAR sz_filename[_MAX_FNAME]{};
-		//	//CHAR sz_filepath[_MAX_PATH] = "starcraft-original\\";
-		//	//SendMessage(hCombo, CB_GETLBTEXT, i, (LPARAM)sz_filename);
-		//	//strcat(sz_filepath, sz_filename);
-		//	//hCursor = (HCURSOR)LoadImage(GetModuleHandle(NULL), sz_filepath, IMAGE_CURSOR, LR_DEFAULTSIZE, LR_DEFAULTSIZE, LR_LOADFROMFILE);
-		//	SetCursor(hCursor);
-		//}
-		//break;
 		case WM_COMMAND:	
 		{
 			switch (LOWORD(wParam))
@@ -126,12 +122,17 @@ LRESULT CALLBACK WndProc(HWND hwnd, UINT uMsg, WPARAM wParam, LPARAM lParam)
 				SendMessage(hCombo, CB_GETLBTEXT, i, (LPARAM)sz_filename);
 				strcat(sz_filepath, sz_filename);
 				hCursor = (HCURSOR)LoadImage(GetModuleHandle(NULL), sz_filepath, IMAGE_CURSOR, LR_DEFAULTSIZE, LR_DEFAULTSIZE, LR_LOADFROMFILE);
-				SetCursor(hCursor);
+				SetCursorPos(500, 300);
+				SetClassLong(hwnd, GCL_HCURSOR, (LONG)hCursor);
+				SetClassLong(GetDlgItem(hwnd, IDC_BUTTON_APPLY), GCL_HCURSOR, (LONG)hCursor);
+				SetClassLong(GetDlgItem(hwnd, IDC_COMBO), GCL_HCURSOR, (LONG)hCursor);
+				//SetCursor(hCursor);
+				return FALSE;
 			}
 			break;
 			}
 		}
-			break;
+		break;
 		case WM_DESTROY:	PostQuitMessage(0); break;
 		case WM_CLOSE:		DestroyWindow(hwnd); break;
 		default:			return DefWindowProc(hwnd, uMsg, wParam, lParam);
@@ -149,4 +150,14 @@ void MoveSizeEvent(HWND hwnd)
 	CHAR sz_buffer[SIZE] = {};
 	sprintf(sz_buffer, "%s: Положение окна (%i x %i) Размер окна (%i x %i)", g_sz_WINDOW_CLASS, rect.left, rect.top, width, height);
 	SendMessage(hwnd, WM_SETTEXT, 0, (LPARAM)sz_buffer);
+}
+
+std::vector<std::string> LoadCursorFromDir(const std::string& directory)
+{
+	std::vector<std::string> files;
+	WIN32_FIND_DATA data;
+	for (HANDLE hFind = FindFirstFile(directory.c_str(), &data); FindNextFile(hFind, &data);) //const char* c_str() - возвращает С-строку, хранящийся в обекте string
+		if(strcmp(strrchr(data.cFileName, '.'), ".cur") == 0 || strcmp(strrchr(data.cFileName, '.'), ".ani") == 0)
+			files.push_back(data.cFileName);
+	return files;
 }

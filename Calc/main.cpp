108f42409f576be3a965dd15e6644f8f9ea963c8
Zello <Zello@DESKTOP-IBMII35>
2023-11-01 23:26:31 +0900

    Normal Calc

--- Calc/main.cpp
+++ Calc/main.cpp
@@ -1,7 +1,26 @@
 #define _CRT_SECURE_NO_WARNINGS
 #include<Windows.h>
+#include"resource.h"
 #include<stdio.h>
 #include<string>
+
+CONST CHAR g_sz_CLASSNAME[] = "Calc";
+
+CONST INT START_X = 10;
+CONST INT START_Y = 10;
+
+
+CONST INT BUTTON_SIZE = 50;
+CONST INT INTERVAL = 5;
+CONST INT BUTTON_DOUBLE_SIZE = BUTTON_SIZE * 2 + INTERVAL;
+
+CONST INT SCREEN_WIDTH = BUTTON_SIZE * 5 + INTERVAL * 4;
+CONST INT SCREEN_HEIGHT = 20;
+
+CONST INT BUTTON_START_X = START_X;
+CONST INT BUTTON_START_Y = START_Y + SCREEN_HEIGHT + INTERVAL * 2;
+
+CONST CHAR* OPERATIONS[] = { "/","*","-","+" };
 
 LRESULT CALLBACK WndProc(HWND hwnd, UINT uMsg, WPARAM wParem, LPARAM lParam);
 
@@ -20,7 +39,7 @@ INT WINAPI WinMain(HINSTANCE hInstance, HINSTANCE hPrevInst, LPSTR lpCmdLine, IN
 	wc.hInstance = hInstance;
 	wc.lpfnWndProc = WndProc;
 	wc.lpszMenuName = NULL;
-	wc.lpszClassName = "Calculation";
+	wc.lpszClassName = g_sz_CLASSNAME;
 
 	if (!RegisterClassEx(&wc))
 	{
@@ -31,13 +50,13 @@ INT WINAPI WinMain(HINSTANCE hInstance, HINSTANCE hPrevInst, LPSTR lpCmdLine, IN
 	HWND hwnd = CreateWindowEx
 	(
 		WS_EX_OVERLAPPEDWINDOW,
-		"Calculation",
-		"Calculation",
+		g_sz_CLASSNAME,
+		g_sz_CLASSNAME,
 		WS_OVERLAPPEDWINDOW,
 		CW_USEDEFAULT,
 		CW_USEDEFAULT,
-		CW_USEDEFAULT,
-		CW_USEDEFAULT,
+		(BUTTON_SIZE+INTERVAL)*5+BUTTON_START_X*3,
+		SCREEN_HEIGHT + (BUTTON_SIZE+INTERVAL)*5 + INTERVAL*2,
 		NULL,
 		NULL,
 		hInstance,
@@ -46,7 +65,7 @@ INT WINAPI WinMain(HINSTANCE hInstance, HINSTANCE hPrevInst, LPSTR lpCmdLine, IN
 
 	if (hwnd == NULL)
 	{
-		MessageBox(NULL, ("Window create failed!"), ("Error"), MB_OK | MB_ICONERROR);
+		MessageBox(NULL, ("Window Create failed!"), ("Error"), MB_OK | MB_ICONERROR);
 		return 0;
 	}
 
@@ -54,7 +73,7 @@ INT WINAPI WinMain(HINSTANCE hInstance, HINSTANCE hPrevInst, LPSTR lpCmdLine, IN
 	UpdateWindow(hwnd);
 
 	MSG msg;
-	while (GetMessage(&msg, NULL, 0, 0) > 0)
+	while (GetMessage(&msg, 0, 0, 0) > 0)
 	{
 		TranslateMessage(&msg);
 		DispatchMessage(&msg);
@@ -64,11 +83,126 @@ INT WINAPI WinMain(HINSTANCE hInstance, HINSTANCE hPrevInst, LPSTR lpCmdLine, IN
 
 LRESULT CALLBACK WndProc(HWND hwnd, UINT uMsg, WPARAM wParam, LPARAM lParam)
 {
-	static HCURSOR hCursor;
 	switch (uMsg)
 	{
-	case WM_CREATE:		break;
-	case WM_COMMAND:	break;
+	case WM_CREATE:		
+	{
+		//Строка ввода
+		HWND hEdit = CreateWindowEx
+		(
+			NULL, "Edit", "0", 
+			WS_CHILDWINDOW | WS_VISIBLE | ES_RIGHT| ES_READONLY | WS_BORDER, 
+			START_X, START_X, SCREEN_WIDTH, SCREEN_HEIGHT,
+			hwnd, (HMENU)IDC_EDIT, GetModuleHandle(NULL), NULL
+		);
+		//Создание цифровой панели
+		int digit = 0;
+		char sz_digit[2] = "";
+		for(int i=6; i>=0; i-=3)
+			for (int j = 0; j < 3; j++)
+			{
+				digit++;
+				sz_digit[0] = digit + 48;
+				CreateWindowEx
+				(
+					NULL, "Button", (LPCSTR)sz_digit, 
+					WS_CHILDWINDOW | WS_VISIBLE |BS_PUSHBUTTON, 
+					BUTTON_START_X + (BUTTON_SIZE+INTERVAL)*j, 
+					BUTTON_START_Y + (BUTTON_SIZE+INTERVAL)*i/3, 
+					BUTTON_SIZE, BUTTON_SIZE, 
+					hwnd, 
+					(HMENU)(IDC_BUTTON_0 + digit), 
+					GetModuleHandle(NULL), NULL
+				);
+			}
+		//Создание нуля
+		CreateWindowEx
+		(
+			NULL, "Button", "0",
+			WS_CHILDWINDOW | WS_VISIBLE | BS_PUSHBUTTON,
+			BUTTON_START_X, BUTTON_START_Y + (BUTTON_SIZE + INTERVAL) * 3,
+			BUTTON_DOUBLE_SIZE, BUTTON_SIZE,
+			hwnd,
+			(HMENU)(IDC_BUTTON_0),
+			GetModuleHandle(NULL), NULL
+		);
+		//Создание точки
+		CreateWindowEx
+		(
+			NULL, "Button", ".",
+			WS_CHILDWINDOW | WS_VISIBLE | BS_PUSHBUTTON,
+			BUTTON_START_X + BUTTON_DOUBLE_SIZE + INTERVAL,
+			BUTTON_START_Y + (BUTTON_SIZE + INTERVAL)*3,
+			BUTTON_SIZE, BUTTON_SIZE,
+			hwnd,
+			(HMENU)(IDC_BUTTON_POINT),
+			GetModuleHandle(NULL), NULL
+		);
+		//Создане операций
+		for (size_t i = 0; i < sizeof(OPERATIONS)/sizeof(OPERATIONS[0]); i++)
+		{
+			CreateWindowEx
+			(
+				NULL, "Button", OPERATIONS[i],
+				WS_CHILDWINDOW | WS_VISIBLE | BS_PUSHBUTTON,
+				BUTTON_START_X + (BUTTON_SIZE + INTERVAL)*3,
+				BUTTON_START_Y + (BUTTON_SIZE + INTERVAL)*i,
+				BUTTON_SIZE, BUTTON_SIZE,
+				hwnd,
+				(HMENU)(IDC_BUTTON_SLASH-i),
+				GetModuleHandle(NULL), NULL
+			);
+		}
+		//Кнопка очистки
+		CreateWindowEx
+		(
+			NULL, "Button", "C",
+			WS_CHILDWINDOW | WS_VISIBLE | BS_PUSHBUTTON,
+			BUTTON_START_X + (BUTTON_SIZE + INTERVAL) * 4,
+			BUTTON_START_Y,
+			BUTTON_SIZE, BUTTON_DOUBLE_SIZE,
+			hwnd,
+			(HMENU)(IDC_BUTTON_CLEAR),
+			GetModuleHandle(NULL), NULL
+		);
+		//Кнопка равно
+		CreateWindowEx
+		(
+			NULL, "Button", "=",
+			WS_CHILDWINDOW | WS_VISIBLE | BS_PUSHBUTTON,
+			BUTTON_START_X + (BUTTON_SIZE + INTERVAL) * 4,
+			BUTTON_START_Y + BUTTON_DOUBLE_SIZE+INTERVAL,
+			BUTTON_SIZE, BUTTON_DOUBLE_SIZE,
+			hwnd,
+			(HMENU)(IDC_BUTTON_EQUAL),
+			GetModuleHandle(NULL), NULL
+		);
+	}
+	break;
+	case WM_COMMAND:	
+	{
+		if (LOWORD(wParam) >= IDC_BUTTON_0 && LOWORD(wParam) <= IDC_BUTTON_9 || LOWORD(wParam) == IDC_BUTTON_POINT)
+		{
+			HWND hEdit = GetDlgItem(hwnd, IDC_EDIT);
+			CHAR sz_buffer[MAX_PATH]{};
+			SendMessage(hEdit, WM_GETTEXT, MAX_PATH, (LPARAM)sz_buffer);
+			if (strcmp(sz_buffer, "0") == 0)
+			{
+				SendMessage(hEdit, WM_SETTEXT, 0, (LPARAM)"");
+				sz_buffer[0] = 0;
+			}
+			CHAR sz_digit[2]{};
+			if (LOWORD(wParam) == IDC_BUTTON_POINT)
+			{
+				if (strchr(sz_buffer, '.')) break;
+				sz_digit[0] = '.';
+			}
+			else sz_digit[0] = LOWORD(wParam) - IDC_BUTTON_0 + 48;
+			strcat(sz_buffer, sz_digit);
+			SendMessage(hEdit, WM_SETTEXT, 0, (LPARAM)sz_buffer);
+		}
+	}
+	break;
 	case WM_DESTROY:	PostQuitMessage(0); break;
 	case WM_CLOSE:		DestroyWindow(hwnd); break;
 	default:			return DefWindowProc(hwnd, uMsg, wParam, lParam);
